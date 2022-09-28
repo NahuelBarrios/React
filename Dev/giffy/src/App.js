00afaa453577e6830b258cfc8dda6fd7ed6ac128
unknown <retro_io@hotmail.com>
2022-09-28 18:04:10 -0300

    Se añadio rutas wouter

--- Dev/giffy/src/App.js
+++ Dev/giffy/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import React, {useEffect,useState} from 'react';
 import './App.css';
 import ListOfGifs from './components/ListOfGifs';
+import {Link, Route} from "wouter"
 
 export default function App() {
 
@@ -10,7 +11,12 @@ export default function App() {
   return (
     <div className="App">
        <section className="App-content">
-      <ListOfGifs keyword='Argentina'/>
+        <h1>Hola</h1>
+        <Link to='/gif/panda'>Gif panda</Link>
+        <Link to='/gif/leon'>Gif Leon</Link>
+        <Link to='/gif/cocodrilo'>Gif cocodrilo</Link>
+      <Route component={ListOfGifs}
+      path="/gif/:keyword" />
       </section>
     </div>
   );
--- Dev/giffy/src/components/ListOfGifs.js
+++ Dev/giffy/src/components/ListOfGifs.js
@@ -3,15 +3,20 @@ import Gif from './Gif'
 import getGifs from '../services/getGifs'
 
 
-export default function ListOfGifs({keyword}){  
+export default function ListOfGifs({params}){  
+  const {keyword} = params;
   const [gifs, setGifs] = useState([]);
 
   useEffect(function (){
     getGifs({keyword}).then(gifs => setGifs(gifs));
   },[keyword])
 
- return  gifs.map(singleGif => 
+ return <div>
+  {
+  gifs.map(singleGif => 
         //<Gif key={singleGif.id} title={singleGif.title} id={singleGif.id} url={singleGif.url}/> 
         <Gif key={singleGif.id} data={singleGif}/> //Mandar los ... cada vez que quiera romper la relacion uwu asi poder modificarlo (Operador spread)
-        )
+      )
+  }
+ </div> 
 }
